c1a5ff1e19fe25745d3e433371de0c8b78191e23
Alex Urdea <alex_urdea_sb@yahoo.com>
2014-01-04 19:59:05 +0000

    proxy-stream tests

---
+++ spec/proxy-stream-spec.js
@@ -0,0 +1,42 @@
+var http = require('http');
+var proxyStream = require('../lib/proxy-stream');
+var ProxyStream = proxyStream.ProxyStream;
+var ERROR_WRITE_STREAM_NOT_INIT = proxyStream.ERROR_WRITE_STREAM_NOT_INIT;
+var helperStreams = require('./helpers/streams');
+var inStream, outStream,
+  pipedValues,
+  contentSaver;
+
+describe('the content saver', function(){
+  beforeEach(function(){
+    pipedValues = ['a', 'b', 'c', 'd', 'e'];
+
+    inStream = new helperStreams.ReadStream({
+      data: pipedValues
+    });
+    outStream = new helperStreams.WriteStream();
+    contentSaver = new ProxyStream();
+  });
+
+
+  it('should pipe data through', function(done){
+    inStream.pipe(contentSaver).pipe(outStream);
+    
+    outStream
+    .on('finish', function(){
+      expect(outStream.data).toEqual(pipedValues);
+      done();
+    });
+  });
+
+
+  it('should be able to intercept JSON responses', function(){
+    var response = new http.IncomingMessage();
+
+    response.headers['content-type'] = 'application/json';
+    expect(contentSaver.intercept(response)).toBe(true);
+
+    response.headers['content-type'] = 'text/html';
+    expect(contentSaver.intercept(response)).toBe(false);
+  });
+});
