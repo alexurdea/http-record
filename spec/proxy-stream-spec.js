7e62ff4d9898f94737da64b4d0f71b429286319d
Alex Urdea <alex_urdea_sb@yahoo.com>
2014-01-07 22:45:42 +0000

    throw in proxy if storage is not set

--- spec/proxy-stream-spec.js
+++ spec/proxy-stream-spec.js
@@ -1,13 +1,37 @@
+var mocks = require('mocks');
+var proxyquire = require('proxyquire');
 var http = require('http');
-var proxyStream = require('../lib/proxy-stream');
-var ProxyStream = proxyStream.ProxyStream;
-var ERROR_WRITE_STREAM_NOT_INIT = proxyStream.ERROR_WRITE_STREAM_NOT_INIT;
 var helperStreams = require('./helpers/streams');
+
 var inStream, outStream,
   pipedValues,
-  contentSaver;
+  prox,
+  trafficPersistMock;
 
-describe('the content saver', function(){
+trafficPersistMock = {
+  createPath: function(){
+    var Q = require(Q);
+    var deferred = Q.defer();
+    var stream = require('stream');
+
+    // mock the fs.WriteStream instance with a stream.Writable instance
+    deferred.resolve(new stream.Writable());
+
+    return deferred.promise;
+  },
+  setBaseDir: function(){ baseDir = ''; }
+};
+
+var proxyStream = proxyquire('../lib/proxy-stream', {
+  trafficPersist: trafficPersistMock,
+  expect: expect
+});
+
+var ProxyStream = proxyStream.ProxyStream;
+var ERROR_WRITE_STREAM_NOT_INIT = proxyStream.ERROR_WRITE_STREAM_NOT_INIT;
+
+
+describe('the proxy stream', function(){
   beforeEach(function(){
     pipedValues = ['a', 'b', 'c', 'd', 'e'];
 
@@ -15,18 +39,7 @@ describe('the content saver', function(){
       data: pipedValues
     });
     outStream = new helperStreams.WriteStream();
-    contentSaver = new ProxyStream();
-  });
-
-
-  it('should pipe data through', function(done){
-    inStream.pipe(contentSaver).pipe(outStream);
-    
-    outStream
-    .on('finish', function(){
-      expect(outStream.data).toEqual(pipedValues);
-      done();
-    });
+    prox = new ProxyStream();
   });
 
 
@@ -34,9 +47,49 @@ describe('the content saver', function(){
     var response = new http.IncomingMessage();
 
     response.headers['content-type'] = 'application/json';
-    expect(contentSaver.intercept(response)).toBe(true);
+    expect(prox.intercept(response)).toBe(true);
 
     response.headers['content-type'] = 'text/html';
-    expect(contentSaver.intercept(response)).toBe(false);
+    expect(prox.intercept(response)).toBe(false);
   });
+
+
+  it('should throw if the storage has not been init', function(done){
+    prox.on('error', function(err){
+      expect(err.message).toEqual(ERROR_WRITE_STREAM_NOT_INIT);
+      done();
+    });
+    inStream.pipe(prox).pipe(outStream);
+  });
+
+  
+  describe('once the storage has been initialized', function(){
+    var storageInitP;
+
+    beforeEach(function(){
+      storageInitP = prox.initStorage('someDir', 'GET', 'http://some.domain.com/');
+      prox.on('error', function(e){
+        throw e;
+      });
+    });
+
+    afterEach(function(){
+      storageInitP = null;
+    });
+
+
+    it('should pipe data through', function(){
+      storageInitP.then(function(){
+        prox
+        .on('finish', function(){
+          expect(outStream.data).toEqual(pipedValues);
+          done();
+        });
+
+        inStream.pipe(prox).pipe(outStream);
+      });
+    });
+  });
+
+
 });
--- spec/traffic-persist-spec.js
+++ spec/traffic-persist-spec.js
@@ -1,5 +1,6 @@
 var trafficPersist;
 var mocks = require('mocks');
+var proxyquire = require('proxyquire');
 var stream = require('stream');
 var Q = require('Q');
 
@@ -7,6 +8,7 @@ describe('The traffic-persist module', function(){
   fs = mocks.fs.create({
     'rootdir': {}
   });
+  // console.log('MFS', fs, '\n.....\n');
   trafficPersist = mocks.loadFile(__dirname + './../lib/traffic-persist', {
     fs: fs,
     expect: expect
@@ -26,35 +28,42 @@ describe('The traffic-persist module', function(){
     });
 
 
-    it('should return a promise', function(){
-      var p = trafficPersist.createPath('GET', 'http://some.domain.com/path/to/some/resource?foo=bar&baz=qux');
-      expect(Q.isPromise(p)).toBe(true);
-    });
-
-
-    it('should create a file hierarchy', function(done){
-      trafficPersist.createPath('GET', 'http://some.domain.com/path/to/some/resource?foo=bar&baz=qux')
-      .then(function complete(writeStream){
-        fs.exists('/rootdir/some.domain.com/path/to/some/get_resource?foo=bar&baz=qux', function(exists){
-          expect(exists).toBe(true);
-          expect(writeStream instanceof fs.WriteStream).toBe(true);
-          done();
-        });
+    describe('createPath', function(){
+      it('should return a promise', function(){
+        var p = trafficPersist.createPath('GET', 'http://some.domain.com/path/to/some/resource?foo=bar&baz=qux');
+        expect(Q.isPromise(p)).toBe(true);
       });
-    });
 
-    
-    it('should use an existing hierarchy', function(done){
-      fs.mkdir('/rootdir/http://some.domain.com/path/', function(err){
-        trafficPersist.createPath('POST', 'http://some.domain.com/path/to/some/resource')
+
+      it('should create a file hierarchy', function(done){
+        // console.log('RFS?', fs, '\n....\n');
+        trafficPersist.setBaseDir('/rootdir');
+        trafficPersist.createPath('GET', 'http://some.domain.com/path/to/some/resource?foo=bar&baz=qux')
         .then(function complete(writeStream){
-          fs.exists('/rootdir/some.domain.com/path/to/some/post_resource', function(exists){
+          // console.log('XX', writeStream);
+          fs.exists('/rootdir/some.domain.com/path/to/some/get_resource?foo=bar&baz=qux', function(exists){
             expect(exists).toBe(true);
             expect(writeStream instanceof fs.WriteStream).toBe(true);
             done();
           });
         });
       });
+
+      
+      it('should use an existing hierarchy', function(done){
+        fs.mkdir('/rootdir/http://some.domain.com/path/', function(err){
+          trafficPersist.createPath('POST', 'http://some.domain.com/path/to/some/resource')
+          .then(function complete(writeStream){
+            fs.exists('/rootdir/some.domain.com/path/to/some/post_resource', function(exists){
+              expect(exists).toBe(true);
+              expect(writeStream instanceof fs.WriteStream).toBe(true);
+              done();
+            });
+          });
+        });
+      });
+
+
     });
 
 
